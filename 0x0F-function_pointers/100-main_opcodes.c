e2e6ad490297122e7fca357cfa5e7c341af10d0c
Henrykoko <henrykokobili@gmail.com>
2022-10-15 14:05:06 -0700

    task 100

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,19 +1,39 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * main - entry point
- * Description: program that prints its own opcodes in two digit hexadecimal
- * lowercase format
- * @argc: argument count
- * @argv: argument vectors
- * Return: 0
+ * print_opcodes - print the opcodes of this program
+ * @a: address of the main function
+ * @n: number of bytes to print
+ *
+ * Return: void
+ */
+
+void print_opcodes(char *a, int n)
+{
+int i;
+
+for (i = 0; i < n; i++)
+{
+printf("%.2hhx", a[i]);
+if (i < n - 1)
+printf(" ");
+}
+printf("\n");
+
+}
+
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: number of arguments passed to the function
+ * @argv: array of pointers to arguments
+ *
+ * Return: always O
  */
 
 int main(int argc, char **argv)
 {
-int n, i;
-int (*ptr)(int, char **);
+int n;
 
 if (argc != 2)
 {
@@ -26,9 +46,6 @@ if (n < 0)
 printf("Error\n");
 exit(2);
 }
-ptr = &main;
-for (i = 0; i < n; i++)
-printf("%.2x ", *((unsigned char *)(ptr + i)));
-printf("\n");
+print_opcodes((char *)&main, n);
 return (0);
 }
